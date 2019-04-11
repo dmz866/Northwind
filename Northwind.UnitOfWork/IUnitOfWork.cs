4749b0717f20395022d53b4254f4ef00c158921c
David Murillo <davidmurillo@Davids-MacBook-Pro.local>
2019-04-10 21:34:43 -0500

    first commit

---
+++ Northwind.UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using Northwind.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Northwind.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        ICustomerRepository Customer { get; }
+    }
+}
